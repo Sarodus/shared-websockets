030e112b729f74eda8c39a597d357f322436ac55
Sarodus <jordiavellapol@gmail.com>
2019-07-07 15:05:18 +0200

    Initial commit

---
+++ test/shared-websocket.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/shared-websocket"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
